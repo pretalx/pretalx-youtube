c9a41b88b3aaf42968e46c068e7b324035efe246
Tobias Kunze <r@rixx.de>
2022-07-11 18:28:46 +0200

    Fix code style

--- pretalx_youtube/urls.py
+++ pretalx_youtube/urls.py
@@ -5,17 +5,17 @@ from .views import YouTubeSettings, api_list, api_single
 
 urlpatterns = [
     re_path(
-        fr"^orga/event/(?P<event>[{SLUG_CHARS}]+)/settings/p/youtube/$",
+        rf"^orga/event/(?P<event>[{SLUG_CHARS}]+)/settings/p/youtube/$",
         YouTubeSettings.as_view(),
         name="settings",
     ),
     re_path(
-        fr"^api/events/(?P<event>[{SLUG_CHARS}]+)/p/youtube/$",
+        rf"^api/events/(?P<event>[{SLUG_CHARS}]+)/p/youtube/$",
         api_list,
         name="api_list",
     ),
     re_path(
-        fr"^api/events/(?P<event>[{SLUG_CHARS}]+)/submissions/(?P<code>[A-Z0-9]+)/p/youtube/$",
+        rf"^api/events/(?P<event>[{SLUG_CHARS}]+)/submissions/(?P<code>[A-Z0-9]+)/p/youtube/$",
         api_single,
         name="api_single",
     ),
