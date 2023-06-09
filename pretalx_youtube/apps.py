90698970a0ed5c5ae2b5049dc869101b6a559de6
Tobias Kunze <r@rixx.de>
2023-06-09 15:19:04 +0200

    Release v1.2.0

--- pretalx_youtube/apps.py
+++ pretalx_youtube/apps.py
@@ -13,7 +13,7 @@ class PluginApp(AppConfig):
             "Embed YouTube videos as session recordings, and retrieve them via an API."
         )
         visible = True
-        version = "1.1.0"
+        version = "1.2.0"
 
     def ready(self):
         from . import signals  # NOQA
