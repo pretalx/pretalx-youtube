6666d382dbba926070ea4bd44e75dd3154368580
Tobias Kunze <r@rixx.de>
2023-09-28 13:29:13 +0200

    Release v2.0.0

--- pretalx_youtube/apps.py
+++ pretalx_youtube/apps.py
@@ -13,7 +13,7 @@ class PluginApp(AppConfig):
             "Show YouTube videos embedded on talk pages. Set URLs manually or via API."
         )
         visible = True
-        version = "1.3.0"
+        version = "2.0.0"
 
     def ready(self):
         from . import signals  # NOQA
