e326c4425c4485db1c093ff0b49e3220229ecc8d
Tobias Kunze <r@rixx.de>
2024-02-16 15:27:11 +0100

    Use SLUG_REGEX

--- pretalx_youtube/urls.py
+++ pretalx_youtube/urls.py
@@ -1,16 +1,16 @@
 from django.urls import re_path
-from pretalx.event.models.event import SLUG_CHARS
+from pretalx.event.models.event import SLUG_REGEX
 from rest_framework import routers
 
 from .api import YouTubeLinkViewSet
 from .views import YouTubeSettings
 
 router = routers.SimpleRouter()
-router.register(f"api/events/(?P<event>[{SLUG_CHARS}]+)/p/youtube", YouTubeLinkViewSet)
+router.register(f"api/events/(?P<event>{SLUG_REGEX})/p/youtube", YouTubeLinkViewSet)
 
 urlpatterns = [
     re_path(
-        rf"^orga/event/(?P<event>[{SLUG_CHARS}]+)/settings/p/youtube/$",
+        rf"^orga/event/(?P<event>{SLUG_REGEX})/settings/p/youtube/$",
         YouTubeSettings.as_view(),
         name="settings",
     ),
