9e803efde56c3c58b515879ca6f28755f61b3ae3
Tobias Kunze <r@rixx.de>
2026-02-19 09:04:44 +0100

    Add tests

---
+++ tests/test_main.py
@@ -0,0 +1,21 @@
+import pytest
+from django.urls import reverse
+
+SETTINGS_URL_NAME = "plugins:pretalx_youtube:settings"
+
+
+@pytest.mark.django_db
+def test_orga_can_access_settings(orga_client, event):
+    response = orga_client.get(
+        reverse(SETTINGS_URL_NAME, kwargs={"event": event.slug}),
+        follow=True,
+    )
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_reviewer_cannot_access_settings(review_client, event):
+    response = review_client.get(
+        reverse(SETTINGS_URL_NAME, kwargs={"event": event.slug}),
+    )
+    assert response.status_code == 404
